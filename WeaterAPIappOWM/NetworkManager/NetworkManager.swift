99996752211c32699bef3c2d9410b80b99173cb6
Vladyslav <bestvineco@gmail.com>
2019-03-20 16:17:56 +0200

    AddFirstFunc

---
+++ WeaterAPIappOWM/NetworkManager/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  WeaterAPIappOWM
+//
+//  Created by Владислав on 3/20/19.
+//  Copyright © 2019 Владислав. All rights reserved.
+//
+
+import Foundation
--- WeaterAPIappOWM/ViewController.swift
+++ WeaterAPIappOWM/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UISearchResultsUpdating {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setupNavigationBar();
     }
-
+    
+    fileprivate func setupNavigationBar(){
+        self.navigationItem.title = "Weather Application";
+        self.navigationController?.navigationBar.prefersLargeTitles = true;
+        
+        let searchController = UISearchController(searchResultsController: nil);
+        searchController.searchResultsUpdater = self;
+        
+        navigationItem.searchController = searchController;
+        navigationItem.hidesSearchBarWhenScrolling = false;
+        
+    }
+    
+    
+    // MARK: - UISearchResultsUpdating
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
+    //MARK: - UITableViewDataSource
 
 }
 
